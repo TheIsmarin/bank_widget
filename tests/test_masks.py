6781d5233aa92e56d4a9e3ba2f28dd2280c7ab58
Ismarin <ilha.2642@gmail.com>
2026-09-23 09:27:28 +0300

    add tests for all modules with 100% coverage

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -7,3 +7,11 @@ def test_get_mask_card_number():
 
 def test_get_mask_account():
     assert get_mask_account("73654108430135874305") == "**4305"
+
+
+def test_get_mask_card_number_invalid_length():
+    assert get_mask_card_number("123") == "123"
+
+
+def test_get_mask_account_invalid_length():
+    assert get_mask_account("123") == "123"
